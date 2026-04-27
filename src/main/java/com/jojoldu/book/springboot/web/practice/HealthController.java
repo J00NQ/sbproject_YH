d04c354a78c1631b84ca4f639297733849665706
User <kjk020913@gmail.com>
2026-04-27 15:48:36 +0900

    [Day01] feat(practice): HealthResponseDto 추가

---
+++ src/main/java/com/jojoldu/book/springboot/web/practice/HealthController.java
@@ -0,0 +1,15 @@
+package com.jojoldu.book.springboot.web.practice;
+
+import com.jojoldu.book.springboot.web.practice.dto.HealthResponseDto;
+import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public HealthResponseDto health() {
+        return new HealthResponseDto("OK", LocalDateTime.now());
+    }
+
+}
