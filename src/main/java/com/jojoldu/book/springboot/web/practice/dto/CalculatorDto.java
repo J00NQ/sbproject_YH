1763a9d0af00f1874f76b7e6ef20e324275ef782
User <kjk020913@gmail.com>
2026-04-27 16:10:49 +0900

    [Day01] feat(practice): CalculatorDto + @Builder

---
+++ src/main/java/com/jojoldu/book/springboot/web/practice/dto/CalculatorDto.java
@@ -0,0 +1,22 @@
+package com.jojoldu.book.springboot.web.practice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class CalculatorDto {
+    private int a;
+    private int b;
+
+    public int add() {
+        return a + b;
+    }
+
+    public int subtract() {
+        return a - b;
+    }
+
+}
