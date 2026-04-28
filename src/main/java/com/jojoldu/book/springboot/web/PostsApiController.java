9edfa4ede7d2ab2cca7bdcd367c623bb4970d31a
User <kjk020913@gmail.com>
2026-04-28 15:36:55 +0900

    [Day02-practice] feat: 작성자별 검색 Service + Controller 추가

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -36,6 +36,11 @@ public class PostsApiController {
         return postsService.findAllDesc();
     }
 
+    @GetMapping("/api/v1/posts/author/{author}")
+    public List<PostsResponseDto> findByAuthor(@PathVariable String author) {
+        return postsService.findByAuthor(author);
+    }
+
     @DeleteMapping("/api/v1/posts/{id}")
     public Long delete(@PathVariable Long id) {
         postsService.delete(id);
