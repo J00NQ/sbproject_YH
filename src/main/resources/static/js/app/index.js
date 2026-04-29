c4f85348dced53c1bd084e85971350698e9d76fc
User <kjk020913@gmail.com>
2026-04-29 16:54:58 +0900

    [Day03-practice] feat: update에서도 빈 값 검증 적용

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -38,6 +38,43 @@ var main = {
             return false;
         }
         return true;
+    },
+    update: function () {
+        var id = $('#id').val();
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+        if (!_this.validateForm(data)) {
+            return;
+        }
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete: function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function () {
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
     }
 };
 
